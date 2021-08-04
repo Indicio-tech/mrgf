65e997382bb1b620ac5efc818837ac5dce12f132
Daniel Bluhm <dbluhm@pm.me>
2021-08-03 21:51:59 -0400

    feat: raise exception on no loaded framework
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- mrgf/acapy.py
+++ mrgf/acapy.py
@@ -1,9 +1,13 @@
 """Elements for integrating with ACA-Py."""
-from typing import Set, cast
+from typing import Set
 from aries_cloudagent.messaging.request_context import RequestContext
 from aries_cloudagent.messaging.responder import BaseResponder
 
 from .governance_framework import GovernanceFramework, Principal
+
+
+class NoLoadedFramework(Exception):
+    """Raised when no framework is loaded."""
 
 
 class RequestHandlerPrincipleFinder:
@@ -14,14 +18,15 @@ class RequestHandlerPrincipleFinder:
     ) -> Principal:
         """Retrieve connection and metadata, evaluate MRGF rules, and return Principal."""
         conn = context.connection_record
-        framework: GovernanceFramework = cast(
-            GovernanceFramework, context.inject(GovernanceFramework)
-        )
+        framework = context.inject(GovernanceFramework, required=False)
+        if not framework:
+            raise NoLoadedFramework(
+                "No machine readable governance framework found in context"
+            )
+
         async with context.session() as session:
             metadata = await conn.metadata_get_all(session)
 
         initial_principal = Principal(id=conn.connection_id, **metadata)
-
         privileges: Set[str] = framework.evaluate(initial_principal)
-
         return Principal(id=conn.connection_id, privileges=privileges, **metadata)
