e137bdd6c25ed780f2910c90366b542049da63fa
Daniel Bluhm <dbluhm@pm.me>
2021-08-26 13:12:01 -0400

    fix: missing pieces of route definition
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- mrgf/routes.py
+++ mrgf/routes.py
@@ -8,9 +8,25 @@ from .governance_framework import GovernanceFramework
 
 
 LOGGER = logging.getLogger(__name__)
+MRGF_RFC = (
+    "https://github.com/hyperledger/aries-rfcs/blob/main/concepts/"
+    "0430-machine-readable-governance-frameworks"
+)
 
 
-@docs(tags=["mrgf"], summary="Set MRGF loaded by agent.")
+@docs(
+    tags=["mrgf"],
+    summary="Set MRGF loaded by agent.",
+    parameters=[
+        {
+            "in": "body",
+            "required": True,
+            "name": "body",
+            "schema": {"type": "object"},
+            "description": "Raw MRGF json",
+        }
+    ],
+)
 async def set_mrgf(request: web.Request):
     """Set MRGF."""
     context: AdminRequestContext = request["context"]
@@ -26,3 +42,28 @@ async def set_mrgf(request: web.Request):
     LOGGER.warning("Replacing MRGF with new MRGF from routes: %s", mrgf)
     context.injector.bind_instance(GovernanceFramework, mrgf)
     return web.json_response({"success": True})
+
+
+async def register(app: web.Application):
+    """Register routes."""
+
+    app.add_routes(
+        [
+            web.post("/mrgf", set_mrgf),
+        ]
+    )
+
+
+def post_process_routes(app: web.Application):
+    """Amend swagger API."""
+
+    # Add top-level tags description
+    if "tags" not in app._state["swagger_dict"]:
+        app._state["swagger_dict"]["tags"] = []
+    app._state["swagger_dict"]["tags"].append(
+        {
+            "name": "mrgf",
+            "description": "Manage Machine Readable Governance Frameworks",
+            "externalDocs": {"description": "Specification", "url": MRGF_RFC},
+        }
+    )
